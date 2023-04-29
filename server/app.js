628d65838b1ac06745cedb359047a885bd9aa222
azaamabubacker <cleancodelk@gmail.com>
2023-04-29 23:47:07 +0530

    Connect MongoDB with mongoose

--- server/app.js
+++ server/app.js
@@ -2,21 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const UserModel = require("./models/Users");
+
 const app = express();
 
+// DB
+mongoose.connect(
+  "mongodb+srv://user123:123@users-cluster.igufzqa.mongodb.net/mern-user-management-app?retryWrites=true&w=majority"
+);
+
 // API
-app.get("/", (req, res, next) => {
-  res.send("Hello World");
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}).then((result) => {
+    res.json(result).catch((err) => {
+      res.json(err);
+    });
+  });
 });
 
-// DB
-mongoose
-  .connect(
-    "mongodb+srv://azaam:123@users-cluster.igufzqa.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then((result) => {
-    app.listen(8080);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+app.listen(8080, () => {
+  console.log("SERVER RUNS SUCCESSFULLY!");
+});
